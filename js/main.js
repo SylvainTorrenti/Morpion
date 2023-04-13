0335797ab4349dca9423f2d4df0a6051a2f93b39
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-04-13 08:45:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const game = document.getElementById("game");
+
+/**
+ * Taille de la grille
+ */
+const sizeOfgrid = 3;
+
+/**
+ * Taille en pixel de la grille
+ */
+const realSizeGrid = 600;
+
+/**
+ * Sauvegarde de l'état de la grille à chaque changement
+ */
+const gridState = [];
+
+
+
+/**
+ * Permet de créer une grille
+ * @param {Number} size Taille de la grille 
+ */
+function CreateGrid(size) {
+
+}
+
+/**
+ * Permet de créer une case de la grille
+ * @param {Number} x Position X
+ * @param {Number} y Position Y
+ */
+function CreateCase(x, y) {
+ 
+}
+
+/**
+ * Permet de créer une nouvelle partie
+ */
+function NewGame() {
+
+}
+
+
+NewGame();
